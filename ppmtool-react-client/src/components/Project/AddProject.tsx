73dca1975abd73d8de19638f6b3c291114b4f72f
Meng Li <meng.li@rwth-aachen.de>
2020-12-05 11:14:13 +0100

    branch 17: handle addProject form event contrl

--- ppmtool-react-client/src/components/Project/AddProject.tsx
+++ ppmtool-react-client/src/components/Project/AddProject.tsx
@@ -1,8 +1,120 @@
-import React, { Component } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
-class AddProject extends Component {
+interface IMyComponentState {
+  projectName: string;
+  projectIdentifier: string;
+  description: string;
+  start_date: string;
+  end_date: string;
+}
+
+class AddProject extends React.Component<{}, IMyComponentState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      start_date: "",
+      end_date: "",
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(
+    e: ChangeEvent<HTMLInputElement> | ChangeEvent<HTMLTextAreaElement>
+  ): void {
+    const key = e.target.name;
+    const value = e.target.value;
+
+    this.setState({ ...this.state, [key]: value });
+  }
+
+  onSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    const project = {
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      start_date: this.state.start_date,
+      end_date: this.state.end_date,
+    };
+
+    console.log(project);
+  }
+
   render() {
-    return <div>Add project works</div>;
+    return (
+      <div className="project">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h5 className="display-4 text-center">Create Project form</h5>
+              <hr />
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg "
+                    placeholder="Project Name"
+                    name="projectName"
+                    value={this.state.projectName}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className="form-control form-control-lg"
+                    placeholder="Unique Project ID"
+                    name="projectIdentifier"
+                    value={this.state.projectIdentifier}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    placeholder="Project Description"
+                    name="description"
+                    value={this.state.description}
+                    onChange={this.onChange}
+                  ></textarea>
+                </div>
+                <h6>Start Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="start_date"
+                    value={this.state.start_date}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <h6>Estimated End Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="end_date"
+                    value={this.state.end_date}
+                    onChange={this.onChange}
+                  />
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
