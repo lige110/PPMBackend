49e18581e7078bef986db95d6c4af68d705a9e71
Meng Li <meng.li@rwth-aachen.de>
2020-12-05 09:55:25 +0100

    branch 16 Finish initial routing in app for/dashBoard /addProject

--- ppmtool-react-client/src/App.tsx
+++ ppmtool-react-client/src/App.tsx
@@ -1,16 +1,24 @@
-import React from 'react';
-import './App.css';
-import Dashborad from './components/Dashborad';
-import Header from './components/Layout/Header';
+import React, { Component } from "react";
+import "./App.css";
+import Dashborad from "./components/Dashborad";
+import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Dashborad />
-    </div>
-  );
+class App extends Component {
+  render() {
+    // need to wrap the whole project up with <Router> tag
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashborad} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ ppmtool-react-client/src/components/Project/AddProject.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AddProject extends Component {
+  render() {
+    return <div>Add project works</div>;
+  }
+}
+
+export default AddProject;
