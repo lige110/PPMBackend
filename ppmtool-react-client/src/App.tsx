c600eb5aef33fa433ced07eddd61eb120025a1a7
Meng Li <meng.li@rwth-aachen.de>
2020-12-05 09:23:06 +0100

    branch 11: React app initial setup

---
+++ ppmtool-react-client/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Dashborad from './components/Dashborad';
+import Header from './components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Dashborad />
+    </div>
+  );
+}
+
+export default App;
