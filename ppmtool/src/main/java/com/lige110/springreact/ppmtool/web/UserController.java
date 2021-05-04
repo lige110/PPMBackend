b28944c89540d22ae4a9edad7d8ddbaac9d2a8e5
Meng Li <meng.li@rwth-aachen.de>
2021-05-04 16:20:53 +0200

    token generated branch 65

--- ppmtool/src/main/java/com/lige110/springreact/ppmtool/web/UserController.java
+++ ppmtool/src/main/java/com/lige110/springreact/ppmtool/web/UserController.java
@@ -2,12 +2,19 @@ package com.lige110.springreact.ppmtool.web;
 
 
 import com.lige110.springreact.ppmtool.domain.User;
+import com.lige110.springreact.ppmtool.payload.JWTLoginSuccessResponse;
+import com.lige110.springreact.ppmtool.payload.LoginRequest;
+import com.lige110.springreact.ppmtool.security.JWTTokenProvider;
 import com.lige110.springreact.ppmtool.services.MapValidationErrorService;
 import com.lige110.springreact.ppmtool.services.UserService;
 import com.lige110.springreact.ppmtool.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,24 +23,49 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+import static com.lige110.springreact.ppmtool.security.SecurityConstants.TOKEN_PREFIX;
+
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
 
+    @Autowired
     private UserService userService;
 
+    @Autowired
     private MapValidationErrorService mapValidationErrorService;
 
+    @Autowired
     private UserValidator userValidator;
 
-    public UserController(@Autowired UserService userService,
-                          @Autowired MapValidationErrorService mapValidationErrorService,
-                          @Autowired UserValidator userValidator) {
-        this.userService = userService;
-        this.mapValidationErrorService = mapValidationErrorService;
-        this.userValidator = userValidator;
-    }
+    @Autowired
+    private JWTTokenProvider tokenProvider;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+
+        if(errorMap!=null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = TOKEN_PREFIX + tokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSuccessResponse(true, jwt));
+
+
+    }
     @PostMapping("/register")
     public ResponseEntity<?> RegisterUser(@Valid @RequestBody User user, BindingResult result){
         // Validate the passwords match
